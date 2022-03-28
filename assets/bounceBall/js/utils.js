c2129ba67e489f9c02d8abbb4cc6f32010f63004
이상일 <sangillee@sangillee-mac.local>
2022-03-28 18:35:56 +0900

    Remove unnecessary app

--- assets/bounceBall/js/utils.js
+++
@@ -1,8 +0,0 @@
-export function getCanvasRelativePosition(e, canvas){
-    const rect = canvas.getBoundingClientRect();
-    console.log(canvas.width/rect.width );
-    return {
-        x: (e.clientX - rect.left) / rect.width  * canvas.width / 2,
-        y: (e.clientY - rect.top ) / rect.height * canvas.height / 2,
-    };
-}
